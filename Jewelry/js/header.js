a0ad7fb48534a72e1eb0cd32c460db02d71442a9
Doug <djw6433@rit.edu>
2017-10-24 16:45:21 -0400

    Added jewelry site

---
+++ Jewelry/js/header.js
@@ -0,0 +1,45 @@
+$(function(){
+  Header_CheckWindowSize();
+  Header_CheckScrollLocation();
+  $("div a.mobile").on("click", function(e){
+    $(this).siblings("ul").stop().slideToggle(500);
+  });
+  $("ul a.mobile").on("click", function(e){
+    $(this).closest("ul").stop().slideToggle(500);
+  });
+
+  $(window).resize(function(){
+    Header_CheckWindowSize();
+    Header_CheckScrollLocation();
+  });
+
+  $(window).scroll(function(e){
+    Header_CheckScrollLocation();
+  });
+
+  function Header_CheckScrollLocation(){
+    var val = $(document).scrollTop().valueOf();
+    if(val > 40){
+      $(".header").removeClass("trans");
+
+    }else{
+      if(!$(".header").hasClass("mobile")){
+        $(".header").addClass("trans");
+      }
+    }
+  }
+
+  function Header_CheckWindowSize(){
+    console.log($(window).width());
+    if($(window).width() <= 900){
+      $(".header").addClass("mobile");
+      $(".header").removeClass("trans");
+      console.log($(".header").children("ul"));
+      $(".header").children("div").children("ul").css("display","none");
+    }else{
+      $(".header").removeClass("mobile");
+      $(".header").addClass("trans");
+      $(".header").children("div").children("ul").css("display","block");
+    }
+  }
+});
